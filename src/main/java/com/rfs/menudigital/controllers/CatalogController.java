340bb55f7dd75fd8f95125f4a259955293d2378b
roselito <roselito@RFS>
2025-12-18 09:16:23 -0300

    Padronização modais e botão cancelar

--- src/main/java/com/rfs/menudigital/controllers/CatalogController.java
+++ src/main/java/com/rfs/menudigital/controllers/CatalogController.java
@@ -44,10 +44,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-/**
- *
- * @author Roselito
- */
 @Controller
 @Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class CatalogController {
